b84fbce64737f599c58043f572388fa6c7da3eda
Paterson <86971279+paterson1@users.noreply.github.com>
2023-06-30 15:54:51 -0300

    fix compile errors

--- contracts/DESAccount.sol
+++ contracts/DESAccount.sol
@@ -12,8 +12,16 @@ import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
 // to call non-view function of system contracts
 import "@matterlabs/zksync-contracts/l2/system-contracts/libraries/SystemContractsCaller.sol";
-
+import { HeartBeat } from "./HeartBeat.sol";
 
 contract DESAccount is DefaultAccount, HeartBeat, IERC1271 {
     // Hook into DefaultAccount execute/validate/etc.
+
+    constructor(address _beneficiary, address _owner) HeartBeat(_beneficiary) public {
+
+    }
+
+     function isValidSignature(bytes32 hash, bytes memory signature) external view returns (bytes4 magicValue){
+
+     }
 }
--- contracts/HeartBeat.sol
+++ contracts/HeartBeat.sol
@@ -4,33 +4,38 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 
-struct Claim {
-    uint256 dateStarted
-}
-
 contract HeartBeat is Ownable {
     address private beneficiary;
-    uint256 timeoutToFinalizeClaim = 1 year; // TODO Setter & constructor
+    bool isActive;
+    uint256 dateStarted;
 
-    constructor(address memory _beneficiary) {
+    constructor(address _beneficiary) {
         beneficiary = _beneficiary;
         // TODO Change owner from factory to account created.
     }
 
-    function heartBeat() public internal onlyOwner {
+    function heartBeat() public onlyOwner {
+        if(!isActive) {
+            dateStarted = 0;
+        }
     }
 
-    function initClaim() public external  {
-        // setDateStarted
+    function initClaim() public   {
+        if(msg.sender == beneficiary) {
+            dateStarted = block.timestamp;
+        }
     }
 
-    function finishClaim() public external  {
-        // if(dateStarted + timeooutToFinalizeClaim > now)
-        // giveAccess as `secondOwner`, meanwhile is a dummyAddress (0x00..00)?
+    function finishClaim() public   {
+        // TODO extract 1 year to var
+        if((dateStarted + 365 days) > block.timestamp) {
+            isActive = true;
+        }
     }
 
-     function kick() public external onlyOwner  {
+     function kick() public onlyOwner  {
         // Remove claim
+        isActive = false;
     }
 
 }
