e12563dcc0499d42886520bbfffad97690d1229c
Paterson <86971279+paterson1@users.noreply.github.com>
2023-07-03 16:21:43 -0300

    change owner

--- contracts/DESAccount.sol
+++ contracts/DESAccount.sol
@@ -2,26 +2,47 @@
 pragma solidity ^0.8.0;
 
 
-import "@matterlabs/zksync-contracts/l2/system-contracts/DefaultAccount.sol";
-import "@openzeppelin/contracts/interfaces/IERC1271.sol";
+import {DefaultAccount } from "./lib/DefaultAccount.sol";
+import { DeadEntitySwitch } from "./DeadEntitySwitch.sol";
 
-// Used for signature validation
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-
-// Access zkSync system contracts for nonce validation via NONCE_HOLDER_SYSTEM_CONTRACT
-import "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
-// to call non-view function of system contracts
-import "@matterlabs/zksync-contracts/l2/system-contracts/libraries/SystemContractsCaller.sol";
-import { HeartBeat } from "./HeartBeat.sol";
-
-contract DESAccount is DefaultAccount, HeartBeat, IERC1271 {
+contract DESAccount is DefaultAccount, DeadEntitySwitch {
     // Hook into DefaultAccount execute/validate/etc.
 
-    constructor(address _beneficiary, address _owner) HeartBeat(_beneficiary) public {
+    constructor(address _beneficiary, address _owner) DeadEntitySwitch(address(this)) public {}
 
+    /// @notice Validation that the ECDSA signature of the transaction is correct.
+    /// @param _hash The hash of the transaction to be signed.
+    /// @param _signature The signature of the transaction.
+    /// @return EIP1271_SUCCESS_RETURN_VALUE if the signaure is correct. It reverts otherwise.
+    function _isValidSignature(bytes32 _hash, bytes memory _signature) internal view override returns (bool) {
+        require(_signature.length == 65, "Signature length is incorrect");
+        uint8 v;
+        bytes32 r;
+        bytes32 s;
+        // Signature loading code
+        // we jump 32 (0x20) as the first slot of bytes contains the length
+        // we jump 65 (0x41) per signature
+        // for v we load 32 bytes ending with v (the first 31 come from s) then apply a mask
+        assembly {
+            r := mload(add(_signature, 0x20))
+            s := mload(add(_signature, 0x40))
+            v := and(mload(add(_signature, 0x41)), 0xff)
+        }
+        require(v == 27 || v == 28, "v is neither 27 nor 28");
+
+        // EIP-2 still allows signature malleability for ecrecover(). Remove this possibility and make the signature
+        // unique. Appendix F in the Ethereum Yellow paper (https://ethereum.github.io/yellowpaper/paper.pdf), defines
+        // the valid range for s in (301): 0 < s < secp256k1n ÷ 2 + 1, and for v in (302): v ∈ {27, 28}. Most
+        // signatures from current libraries generate a unique signature with an s-value in the lower half order.
+        //
+        // If your library generates malleable signatures, such as s-values in the upper range, calculate a new s-value
+        // with 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFEBAAEDCE6AF48A03BBFD25E8CD0364141 - s1 and flip v from 27 to 28 or
+        // vice versa. If your library also generates signatures with 0/1 for v instead 27/28, add 27 to v to accept
+        // these malleable signatures as well.
+        require(uint256(s) <= 0x7FFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF5D576E7357A4501DDFE92F46681B20A0, "Invalid s");
+
+        address recoveredAddress = ecrecover(_hash, v, r, s);
+
+        return recoveredAddress == owner && recoveredAddress != address(0);
     }
-
-     function isValidSignature(bytes32 hash, bytes memory signature) external view returns (bytes4 magicValue){
-
-     }
 }
