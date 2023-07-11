39018b4bd2fe2f74e7044a2c441512b46f2d5ae6
Paterson <86971279+paterson1@users.noreply.github.com>
2023-07-11 11:24:22 -0300

    event on factory

--- contracts/DESAccountFactory.sol
+++ contracts/DESAccountFactory.sol
@@ -1,33 +1,41 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.17;
 
-
 import "@matterlabs/zksync-contracts/l2/system-contracts/Constants.sol";
 import "@matterlabs/zksync-contracts/l2/system-contracts/libraries/SystemContractsCaller.sol";
 
 contract DESAccountFactory {
-    bytes32 public aaBytecodeHash;
+  bytes32 public aaBytecodeHash;
 
-    constructor(bytes32 _aaBytecodeHash) {
-        aaBytecodeHash = _aaBytecodeHash;
-    }
+  event DESAccountDeployed(address indexed _newAccount, address _owner);
 
-    function deployAccount(
-        bytes32 salt,
-        address owner
-    ) external returns (address accountAddress) {
-        (bool success, bytes memory returnData) = SystemContractsCaller
-            .systemCallWithReturndata(
-                uint32(gasleft()),
-                address(DEPLOYER_SYSTEM_CONTRACT),
-                uint128(0),
-                abi.encodeCall(
-                    DEPLOYER_SYSTEM_CONTRACT.create2Account,
-                    (salt, aaBytecodeHash, abi.encode(owner), IContractDeployer.AccountAbstractionVersion.Version1)
-                )
-            );
-        require(success, "Deployment failed");
+  constructor(bytes32 _aaBytecodeHash) {
+    aaBytecodeHash = _aaBytecodeHash;
+  }
 
-        (accountAddress) = abi.decode(returnData, (address));
-    }
+  function deployAccount(
+    bytes32 salt,
+    address owner
+  ) external returns (address accountAddress) {
+    (bool success, bytes memory returnData) = SystemContractsCaller
+      .systemCallWithReturndata(
+        uint32(gasleft()),
+        address(DEPLOYER_SYSTEM_CONTRACT),
+        uint128(0),
+        abi.encodeCall(
+          DEPLOYER_SYSTEM_CONTRACT.create2Account,
+          (
+            salt,
+            aaBytecodeHash,
+            abi.encode(owner),
+            IContractDeployer.AccountAbstractionVersion.Version1
+          )
+        )
+      );
+    require(success, "Deployment failed");
+
+    (accountAddress) = abi.decode(returnData, (address));
+    
+    emit DESAccountDeployed(accountAddress, owner);
+  }
 }
