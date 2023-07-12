8afcba5960d2d5c12d3a262240a8647ef8b4105f
Paterson <86971279+paterson1@users.noreply.github.com>
2023-07-12 15:39:07 -0300

    remove dependencies

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,7 +4,6 @@ import "@matterlabs/hardhat-zksync-deploy";
 import "@matterlabs/hardhat-zksync-solc";
 
 import "@matterlabs/hardhat-zksync-verify";
-import "@matterlabs/hardhat-zksync-chai-matchers";
 
 // dynamically changes endpoints for local tests
 const zkSyncTestnet =
